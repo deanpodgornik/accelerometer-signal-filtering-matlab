868c57d4550da20f420b0afce5ca04c9504b28ca
Dean Podgornik <dean.podgornik@gmail.com>
2014-06-16 19:07:19 +0200

    Os Z implementirana. Dokončno

--- 1/kalman/Popravek_hitrosti_os_z.m
+++ 1/kalman/Popravek_hitrosti_os_z.m
@@ -3,24 +3,12 @@ function [hitrost, firstRun_os_z] = Popravek_hitrosti_os_z( firstRun_os_z, i, hi
     
     persistent max;
     
-    pragObmocjaNapakeUstavljanja = 0.4;
-    
     if(firstRun_os_z == 1)
         firstRun_os_z = 0;
     end
     
     hitrost_raw = hitrost + popravek_hitrosti_num;
-    
-    if(firstRun_os_z == 1 || iteracija_gibanja == 1 || zadetekMejeSlike == 1)
-        max = hitrost_raw;
-    end
-    
-    %pridobitev max
-    if(hitrost_raw > max)
-        max = hitrost_raw;
-    end
-    
-    hitrost_brez_filtriranja_nizkih_frekvenc = hitrost;
+
     %filtriranje nizkih frekvenc hitrosti
     if(abs(hitrost)<0.15)
         hitrost = 0;
@@ -32,17 +20,4 @@ function [hitrost, firstRun_os_z] = Popravek_hitrosti_os_z( firstRun_os_z, i, hi
     else
         hitrost = hitrost * 1.5;
     end
-    
-    if(i > 560)
-        i
-    end
-    
-    %preverim ali meritev spada v omboèje popravljanja napake ustavljanja
-    %if(hitrost > 0 && ((hitrost_raw - popravek_hitrosti_num) < ((max - popravek_hitrosti_num) * pragObmocjaNapakeUstavljanja)))
-    if(hitrost_brez_filtriranja_nizkih_frekvenc > 0 && ((hitrost_raw - popravek_hitrosti_num) < ((max-popravek_hitrosti_num) * pragObmocjaNapakeUstavljanja)))
-        i
-        popravek_hitrosti_num = hitrost_raw;
-        %hitrost = 0;
-    end   
 end
-
